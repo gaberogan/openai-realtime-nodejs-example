c0934d23c20dcf00af28b7460b31ee985dc8a159
Gabe Rogan <gabe@futo.org>
2024-12-31 10:16:52 -0500

    cleanup

--- wake.js
+++ wake.js
@@ -1,20 +1,5 @@
-// Copyright 2022 David Scripka. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import * as ort from 'onnxruntime-node'
 import { spawn } from 'child_process'
-import path from 'path'
 
 // Configuration
 const CHUNK_SIZE = 96
@@ -30,11 +15,7 @@ class WakeWordDetector {
   async loadModel() {
     try {
       // Load ONNX model
-      const modelPath = path.join(
-        process.env.HOME,
-        '.pyenv/versions/jarvis/lib/python3.12/site-packages/openwakeword/resources/models/hey_jarvis_v0.1.onnx',
-      )
-      this.session = await ort.InferenceSession.create(modelPath)
+      this.session = await ort.InferenceSession.create('./hey_jarvis_v0.1.onnx')
 
       // Initialize prediction buffer
       this.predictionBuffer.set(this.modelName, Array(20).fill(0))
