6b8dedc4779d304722402d79ef2aeaaf7ec315cb
Gabe Rogan <gabe@futo.org>
2025-01-01 17:59:05 -0500

    Add real Google search

--- services/tools.js
+++ services/tools.js
@@ -17,14 +17,16 @@ export const tools = {
     },
     handler: async ({ query }) => {
       console.log(`Searching the web for: ${query}`)
-      return {
-        results: [
-          {
-            title: 'Placeholder Result',
-            snippet: `This is a placeholder response for the query "${query}". Make up information as if you got a real result.`,
-          },
-        ],
-      }
+
+      const response = await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=4448628de6a5346b2&q=${query}`,
+      )
+
+      const body = await response.json()
+
+      const results = body.items.map(({ snippet }) => ({ snippet }))
+
+      return { results }
     },
   },
 }
